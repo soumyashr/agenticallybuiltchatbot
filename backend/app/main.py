5a9fb36016a1d5c4ed6c60226bbb16dbdb4152d0
soumyalearning <soumshr@gmail.com>
2026-03-07 00:16:42 +0530

    Add frontend App Runner URL to CORS allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:3001", "https://gazfq7ai7a.ap-south-1.awsapprunner.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
